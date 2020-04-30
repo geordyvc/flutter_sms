1b66cc18dd3d1ca412ece3543135898caeb7156a
Andrei Miulescu <lusu777@gmail.com>
2020-05-01 09:23:59 +1000

    Permission handler

--- android/src/main/java/com/babariviere/sms/SmsRemover.java
+++ android/src/main/java/com/babariviere/sms/SmsRemover.java
@@ -18,10 +18,19 @@ public class SmsRemover implements PluginRegistry.RequestPermissionsResultListen
 	private final PluginRegistry.Registrar registrar;
 	private final Permissions permissions;
 
+	private final String[] permissionsList = new String[] { Manifest.permission.READ_SMS, Manifest.permission.WRITE_SMS,
+			Manifest.permission.READ_PHONE_STATE };
+
 	SmsRemover(PluginRegistry.Registrar registrar) {
 		this.registrar = registrar;
 		this.permissions = new Permissions(registrar.activity());
 		registrar.addRequestPermissionsResultListener(this);
+	}
+
+	void handle(Permissions permissions, String fromAddress) {
+		if (permissions.checkAndRequestPermission(permissionsList, Permissions.SEND_SMS_ID_REQ)) {
+			deleteSms(fromAddress);
+		}
 	}
 
 	private boolean deleteSms(String fromAddress) {
@@ -55,7 +64,7 @@ public class SmsRemover implements PluginRegistry.RequestPermissionsResultListen
 			case "removeSms":
 				if (methodCall.hasArgument("fromAddress")) {
 					Log.i("SMSREMOVER", "method called for removing sms: " + methodCall.argument("fromAddress"));
-					result.success(this.deleteSms(methodCall.argument("fromAddress").toString()));
+					handler.handle(this.permissions, methodCall.argument("fromAddress").toString());
 				}
 		}
 
@@ -63,7 +72,7 @@ public class SmsRemover implements PluginRegistry.RequestPermissionsResultListen
 
 	@Override
 	public boolean onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-		if (requestCode != Permissions.READ_SMS) {
+		if (requestCode != Permissions.READ_SMS_ID_REQ) {
 			return false;
 		}
 		boolean isOk = true;
